916308c335c5bd9e0f5c8d9600eaff10c5674494
akadon <roreto1001@gmail.com>
2022-09-30 01:32:58 +0000

    vercel

--- components/Helper.tsx
+++ components/Helper.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axifetch = async (url: any) => {
+  if (process.env.VERCEL_URL != null && !(url.includes("http") || url.includes("https"))) { url = "https://" + url + process.env.VERCEL_URL; }
   return await axios.create({}).get(url).then((res: any) => { console.log("axi", res); return res.data });
 }
